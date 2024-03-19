6db73f19f735576b4369c52dbe5188886a9126a5
liuhong <lj5525908@gmail.com>
2024-03-19 15:12:54 +0800

    feat: Required query parameters

--- main.py
+++ main.py
@@ -48,15 +48,7 @@ async def read_item(item_id: str, q: str | None = None, short: bool = False):
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
 
-@app.get("/users/{user_id}/items{item_id}")
-async def read_user_item(
-    user_id: int, item_id: str, q: str | None = None, short: bool = False
-):
-    item = {"item_id": item_id, "owner_id": user_id}
-    if q:
-        item.update({"q": q})
-    if not short:
-        item.update(
-            {"description": "This is an amazing item that has a long description"}
-        )
+@app.get("/items/{item_id}")
+async def read_user_item(item_id: str, needy: str):
+    item = {"item_id": item_id, "needy": needy}
     return item
