bf900837e228b88313b6ef990520cc363aa3e1f5
liuhong <lj5525908@gmail.com>
2024-03-19 18:27:16 +0800

    feat: Default values

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 
 @app.get("/items/")
-async def read_items(q: Annotated[str | None, Query(max_length=50)] = None):
+async def read_items(q: Annotated[str, Query(min_length=3)] = "fixedquery"):
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
         results.update({"q": q})
