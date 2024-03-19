8ac34abda7652aaee59099b7d1ab0b362a9bf362
liuhong <lj5525908@gmail.com>
2024-03-19 15:26:15 +0800

    feat: Required Parameters and default Parameters.

--- main.py
+++ main.py
@@ -49,6 +49,8 @@ def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
 
 @app.get("/items/{item_id}")
-async def read_user_item(item_id: str, needy: str):
-    item = {"item_id": item_id, "needy": needy}
+async def read_user_item(
+    item_id: str, needy: str, skip: int = 0, limit: int | None = None
+):
+    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
     return item
