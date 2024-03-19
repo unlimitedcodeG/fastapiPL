9ce3e1800210fcb9068cdaeba449b526275c5775
liuhong <lj5525908@gmail.com>
2024-03-19 15:47:00 +0800

    feat: Query Parameters and String Validations

--- main.py
+++ main.py
@@ -1,56 +1,11 @@
-from typing import Union
-
 from fastapi import FastAPI
-from pydantic import BaseModel 
 
 app = FastAPI()
 
 
-fake_items_db = [
-    {"item_name": "Foo"},
-    {"item_name": "Bar"},
-    {"item_name": "Baz"},
-]
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: bool| None
-    model_config = {
-    "json_schema_extra": {
-        "examples": [
-            {
-                "name": "Foo",
-                "description": "A very nice Item",
-                "price": 35.4,
-                "tax": 3.2,
-            }
-        ]
-    }
-}
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-# @app.get("/items/{item_id}")
-# async def read_item(item_id: str, q: str | None = None, short: bool = False):
-#     item = {"item_id": item_id}
-#     if q:
-#         item.update({"q": q})
-#     if not short:
-#         item.update(
-#             {"description": "This is an amazing item that has a long description"}
-#         )
-#     return item
-    
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
-
-@app.get("/items/{item_id}")
-async def read_user_item(
-    item_id: str, needy: str, skip: int = 0, limit: int | None = None
-):
-    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
-    return item
+@app.get("/items/")
+async def read_items(q: str | None = None):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
