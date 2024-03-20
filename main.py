567ef328476bf2d1e8de4db4adafa058a6320e68
liuhong <lj5525908@gmail.com>
2024-03-20 10:45:46 +0800

    feat：Annotate Path

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from typing import Annotated
 
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Path, Query
 
 app = FastAPI()
 
 
-@app.get("/items/")
-async def read_items(q: Annotated[str, Query(min_length=3)] = "fixedquery"):
-    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+@app.get("/items/{item_id}")
+async def read_items(
+    item_id: Annotated[int, Path(title="The ID of the item to get")],
+    q: Annotated[str | None, Query(alias="item-query")] = None,
+):
+    results = {"item_id": item_id}
     if q:
         results.update({"q": q})
     return results
