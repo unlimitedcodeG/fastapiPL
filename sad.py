ba265b76592c53bd30783437bbb901108bfba591
unlimitedcodeG <1659159807@qq.com>
2024-05-14 16:38:11 +0800

    feat: rotational_substring

--- sad.py
+++ sad.py
@@ -1,1 +1,10 @@
-print("sassd")
+def is_rotational_substring(s1,s2):
+    if not s1 or not s2:
+        return False
+    s1s2 = s1+s2 
+    return s1 in s1s2
+
+# 测试
+s1 = "abcd"
+s2 = "dabcabc"
+print(is_rotational_substring(s1, s2))  # 输出: True
